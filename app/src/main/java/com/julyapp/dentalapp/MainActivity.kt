7366e643d422ccb7c89277df8c9e49599416fbdb
muhmasykur@outlook.com <muhmasykur@outlook.com>
2021-02-03 08:09:02 +0800

    fb login dan gmail fix on activity_main.xml.xml

--- app/src/main/java/com/julyapp/dentalapp/MainActivity.kt
+++ app/src/main/java/com/julyapp/dentalapp/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.julyapp.dentalapp
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.Log
+import android.util.Patterns
+import android.view.View
 import android.widget.Toast
 import com.facebook.*
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.Login
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.julyapp.dentalapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import java.util.*
+import java.util.regex.Pattern
 
+
+    const val R_CODE_GSIGN_IN = 1
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     lateinit var auth: FirebaseAuth
     val callbackManager = CallbackManager.Factory.create()
@@ -23,6 +45,18 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = "ClassName"
     }
+    val PassPattern =
+        Pattern.compile(
+            "^" +
+                    //"(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      //any letter
+//                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+//                    "(?=\\S+$)" +           //no white spaces
+                    ".{6,}" +               //at least 4 characters
+                    "$"
+        );
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +68,36 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
 
+        val ss = SpannableString("Sudah Punya Akun? Masuk Disini")
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(textView: View) {
+                Toast.makeText(this@MainActivity, "pindah nnti", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                ds.isFakeBoldText = true
+                ds.color = Color.rgb(59, 163, 204)
+            }
+
+        }
+        ss.setSpan(clickableSpan, 24, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.tvDaftar.text = ss
+        binding.tvDaftar.movementMethod = LinkMovementMethod.getInstance()
+        binding.tvDaftar.highlightColor = Color.TRANSPARENT
+        binding.btLogin.setOnClickListener {
+
+            if (!validateForm()) {
+                return@setOnClickListener
+            }
+            registerUser(
+                binding.editemail.text.toString(),
+                binding.editpass.text.toString(),
+                binding.editUsername.text.toString()
+            )
+        }
+        
         binding.Fblogin.setOnClickListener {
             LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"))
             LoginManager.getInstance().registerCallback(callbackManager, object :
@@ -58,13 +122,53 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        binding.Glogin.setOnClickListener {
+            val option = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.ClietID))
+                .requestEmail()
+                .build()
+
+            val signInClient = GoogleSignIn.getClient(this, option)
+            signInClient.signInIntent.also {
+                startActivityForResult(it, R_CODE_GSIGN_IN)
+            }
+
+        }
+
+
 
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        if (requestCode === R_CODE_GSIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthFirebase(it)
+            }
+        }
         // Pass the activity result back to the Facebook SDK
         callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    private fun googleAuthFirebase(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credential).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "login berhasil", Toast.LENGTH_SHORT).show()
+                }
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.toString(), Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+
     }
     private fun handleFacebookAccessToken(token: AccessToken) {
         Log.d(TAG, "handleFacebookAccessToken:$token")
@@ -86,5 +190,69 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val pass = binding.editpass.text.toString().trim()
+        val confPass = binding.editconfpass.text.toString().trim()
+        if (pass.isEmpty()) {
+            binding.outlinetvPass.error = "Masukkan Password"
+            valid = false;
+        } else if (confPass.isEmpty()) {
+            binding.outlinetvConfPass.error = "Masukkan Konfirmasi Password"
+            valid = false;
+        } else if (pass != confPass) {
+            Toast.makeText(this, "$pass ,$confPass", Toast.LENGTH_SHORT).show()
+            binding.outlinetvConfPass.error = "Konfirmasi Password Salah"
+            binding.editconfpass.text?.clear()
+            valid = false;
+        }
+
+
+        var userName = binding.editUsername.text.toString().trim()
+        if (userName.isEmpty()) {
+            binding.outlinetvConfPass.error = "Masukkan Username"
+            valid = false;
+        } else if (userName.length > 15) {
+            binding.outlinetvUsername.error = "Username terlalu panjang"
+            valid = false;
+        }
+        val email = binding.editemail.text.toString().trim()
+        if (email.isEmpty()) {
+            binding.outlinetvEmail.error = "Masukkan Email"
+            valid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.outlinetvEmail.error = "Email Salah"
+            valid = false;
+        }
+        return valid
+    }
+
+
+    private fun registerUser(email: String, pass: String, username: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.createUserWithEmailAndPassword(email,pass).await()
+                val user = auth.currentUser
+                user.let {
+                    val profileUpdate = UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build()
+                    user?.updateProfile(profileUpdate)?.await()
+                }
+
+                withContext(Dispatchers.Main) {
+
+                    Toast.makeText(this@MainActivity, "pindah nnti", Toast.LENGTH_SHORT).show()
+                }
+            }catch (e:Exception){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 }
